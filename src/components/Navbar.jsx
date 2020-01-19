cf178217e6e78fce28ed01fefe95c448556cf274
Jonathan Northcott <northcott.j@chisigma.co>
2020-01-19 16:21:34 -0500

    fixing build errors (#5)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,6 @@ class NavBar extends Component {
   state = {}
 
   render() {
-    const { activeItem } = this.state
-
     return (
       <Menu stackable borderless size='huge' >
         <Menu.Item
