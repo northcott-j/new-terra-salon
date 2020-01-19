fdc0f10ed7ec17d04644495b35e3117c3481b9b0
Jonathan Northcott <northcott.j@chisigma.co>
2020-01-19 14:18:45 -0500

    React Helmet Setup (#3)
    * fixing travis master deploy condition
    
    * setting up React Helmet for header info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Terra Salon | Nashua, NH</title>
+        <meta name="description" content="Terra is a full service salon
+        located in Nashua, NH that is committed to providing earth friendly hair care services." />
+        <link rel="canonical" href="http://www.terrasalonnh.com/" />
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
