238544566a78d2cf3db448405472fbf71fa58379
Jonathan Northcott <northcott.j@chisigma.co>
2020-01-22 23:40:53 -0500

    making navbar links icons on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,79 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Menu, Image } from 'semantic-ui-react';
+import { Menu, Image, Responsive, Icon } from 'semantic-ui-react';
 import logo from '../logo.png';
 
 class NavBar extends Component {
-  state = {}
+  state = {
+    menuItems: [
+      {
+        name: 'services',
+        color: 'grey',
+        to: '/services',
+        text: 'Services',
+        icon: 'sitemap'
+      },
+      {
+        name: 'ourTeam',
+        color: 'grey',
+        to: '/our-team',
+        text: 'Our Team',
+        icon: 'users'
+      },
+      {
+        name: 'contactUs',
+        color: 'grey',
+        to: '/contact-us',
+        text: 'Contact Us',
+        icon: 'comments'
+      }
+    ]
+  }
 
   render() {
+    const renderMenuItems = (isMobile) => {
+      const style = (isMobile) ? {flex: 1} : {}
+        return this.state.menuItems.map((menuItem) =>
+          <Menu.Item
+            as={NavLink}
+            name={ menuItem.name }
+            color={ menuItem.color }
+            to={ menuItem.to }
+            style={ style }
+            >
+            { (isMobile) ? <Icon className="clear-margins"
+                                 size="large"
+                                 name={ menuItem.icon } /> : menuItem.text }
+          </Menu.Item>
+        );
+    };
+
+    const renderMenu = (isMobile) => {
+      return (
+        <Menu borderless={ !isMobile } size='huge'>
+          <Menu.Item
+            name='home'
+            href='/#/'
+            className='navbar-logo'
+            >
+            <Image src={logo} alt="Terra Salon Logo" size='small' />
+          </Menu.Item>
+          <Menu.Menu position='right' icon={ isMobile }>
+            { renderMenuItems(isMobile) }
+          </Menu.Menu>
+        </Menu>
+      )
+    }
+
     return (
-      <Menu stackable borderless size='huge' >
-        <Menu.Item
-          name='home'
-          href='/#/'
-          >
-          <Image src={logo} alt="Terra Salon Logo" size='small' />
-        </Menu.Item>
-
-        <Menu.Menu position='right'>
-          <Menu.Item
-            as={NavLink}
-            name='services'
-            color="grey"
-            to='/services'
-            >
-            Services
-          </Menu.Item>
-
-          <Menu.Item
-            as={NavLink}
-            name='ourTeam'
-            color="grey"
-            to='/our-team'
-            >
-            Our Team
-          </Menu.Item>
-
-          <Menu.Item
-            as={NavLink}
-            name='contactUs'
-            color="grey"
-            to='/contact-us'
-            >
-            Contact Us
-          </Menu.Item>
-        </Menu.Menu>
-      </Menu>
+      <div>
+        <Responsive {...Responsive.onlyMobile}>
+          { renderMenu(true) }
+        </Responsive>
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+          { renderMenu(false) }
+        </Responsive>
+      </div>
     )
   }
 }
